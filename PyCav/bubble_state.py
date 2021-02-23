fix up the T averaging

PyCav/bubble_state.py
@@ -91,14 +91,12 @@ class bubble_state:
 
     def init_pdf(self):
         if self.shape == "lognormal":
-            print('lognormal')
             self.f = sp.lognorm.pdf(self.R0, self.sigR0, loc=np.log(self.muR0))
         elif self.shape == "normal":
-            print('normal')
             self.f = sp.norm.pdf(self.R0, scale=self.sigR0, loc=self.muR0)
         else:
             raise NotImplementedError
-        if (max(abs(self.f)) < 1.e-6):
+        if max(abs(self.f)) < 1.e-6:
             raise Exception('all PDF points are small')
 
     def init_GH(self):
@@ -240,7 +238,7 @@ class bubble_state:
                         Nt = len(vals[:,0,0])
                         # G = np.zeros(self.NR0)
                         G = get_G(vals=vals,
-                                mom=mom,
+                                mom=np.array(mom),
                                 Nt=Nt,
                                 shifts=shifts,
                                 NR0=self.NR0)

PyCav/moments.py
@@ -31,7 +31,9 @@ def get_moment(sol):
                 # Get target time steps
                 jTargets = []
                 current_time = sol.times[j]
-                t_target = current_time - sol.state.periods
+                t_target = \
+                        current_time - \
+                        sol.state.Tfilt * sol.state.periods
                 # print('t_targets: ', t_target)
                 for k in range(sol.state.NR0):
                     q = np.argmin(np.abs(sol.times - t_target[k]))
@@ -52,8 +54,9 @@ def get_moment(sol):
                 jShift = np.array(jTargets) - j
                 # print('jShift: ', jShift)
 
+                # print('js',jMin,j)
                 mom[j, :] = sol.state.get_quad(
-                    vals=sol.save[jMin : j], 
+                    vals=sol.save[jMin : j+1], 
                     filt=True,
                     Tfilt=True,
                     shifts=jShift
@@ -71,13 +74,18 @@ def get_G(vals,
         shifts,
         NR0):
     G = np.zeros(NR0)
+    # print(vals)
+    # print(np.shape(vals))
     for i in range(NR0):
         G[i] = 0.
-        for q in range(Nt-1,Nt-1+shifts[i],-1):
+        # print('shifts', shifts)
+        # print('Nt ->',Nt-1,Nt-1+shifts-1)
+        for q in range(Nt-1,Nt-1+shifts[i]-1,-1):
             G[i] += (
                     vals[q, i, 0] ** mom[0] * 
                     vals[q, i, 1] ** mom[1]
                 )
         G[i] /= (abs(shifts[i]) + 1.)
 
+    # print('G = ',G)
     return G

PyCav/waveforms.py
@@ -50,7 +50,7 @@ class waveforms:
     def p_sine(self, t):
         if t <= self.period * self.cycles:
             f = 2.0 * np.pi / self.period
-            return [self.amplitude * np.sin(f * t), self.amplitude * np.cos(f * t) * f]
+            return [self.ambient + self.amplitude * np.sin(f * t), self.amplitude * np.cos(f * t) * f]
         else:
             return [self.ambient, 0.0]
 